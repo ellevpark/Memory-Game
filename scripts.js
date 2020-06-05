c97bd790201bfc8524c05a65227bec02adbb68ef
Ellen Park <ellenpark781@gmail.com>
2020-06-05 17:01:27 -0400

    created function to randomly shuffle the cards

--- scripts.js
+++ scripts.js
@@ -1,10 +1,15 @@
 const cards = document.querySelectorAll('.memory-card'); 
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard; 
 
 function flipCard() {
+  if (lockBoard) return;
+  if (this===firstCard) return;
+
   this.classList.add('flip');
+
   if (!hasFlippedCard){
     //first click
     hasFlippedCard = true;
@@ -15,6 +20,7 @@ function flipCard() {
     //second click
     hasFlippedCard = false;
     secondCard = this; 
+
     checkMatch();
 }
 
@@ -27,13 +33,29 @@ function checkMatch() {
 function disableCards(){
   firstCard.removeEventListener('click', flipCard);
   secondCard.removeEventListener('click', flipCard);
+  
+  resetBoard();
 }
 
 function unflipCards(){ 
+  lockBoard = true; 
   setTimeout(() => {
     firstCard.classList.remove('flip'); 
     secondCard.classList.remove('flip');
+    resetBoard();
   }, 500)
 }
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false]
+  [firstCard, secondCard] = [null, null]
+}
+
+(function shuffle() {
+  cards.forEach(card => {
+    let randomize = Math.floor(Math.random() * 12);
+    card.style.order = randomize;
+  });
+})();
 
 cards.forEach(card => card.addEventListener('click', flipCard))
